1c41ce24bbc02d286e8864c6a349cc3b86d021c6
Mark Battistella <mark_battistella@hotmail.com>
2019-10-22 17:32:34 +1100

    Initial Commit

---
+++ mbTrafficLightButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mbTrafficLightButton
+//
+//  Created by Mark Battistella on 22/10/19.
+//  Copyright © 2019 Mark Battistella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
