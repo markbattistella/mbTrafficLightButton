a6e6a952d3e463608e743dc25a36b75ba49044e9
markbattistella <markb@markbattistellafilms.com>
2019-10-23 14:43:46 +1100

    cleanup

--- mbTrafficLightButton/ViewController.swift
+++ mbTrafficLightButton/ViewController.swift
@@ -8,78 +8,84 @@
 
 import UIKit
 
+// create global colours
 extension UIColor {
-    static var redOff       = UIColor(red: 255/255, green: 0/255,   blue: 0/255, alpha: 0.5 )
-    static var yellowOff    = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 0.5 )
-    static var greenOff     = UIColor(red: 0/255,   green: 255/255, blue: 0/255, alpha: 0.5   )
-
-    static var black50      = UIColor(red: 0/255,   green: 0/255,   blue: 0/255, alpha: 0.5   )
+	static let redOff       = UIColor(red: 255/255, green: 0/255,   blue: 0/255, alpha: 0.25 )
+	static let yellowOff    = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 0.25 )
+	static let greenOff     = UIColor(red: 0/255,   green: 255/255, blue: 0/255, alpha: 0.25 )
 }
 
-
 class ViewController: UIViewController {
-
-    // set the variables
-    @IBOutlet var redLight      : UIButton!
-    @IBOutlet var yellowLight   : UIButton!
-    @IBOutlet var greenLight    : UIButton!
-    @IBOutlet var buttonText    : UIButton!
-    
-    var colourCounter   = 1
-
-    
+	
+	// set the variables
+	@IBOutlet var redLight      : UIImageView!
+	@IBOutlet var yellowLight   : UIImageView!
+	@IBOutlet var greenLight    : UIImageView!
+	@IBOutlet var buttonText    : UIButton!
+	var colourCounter   = 0
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-        // run the colour switcher
-        colourSwitcher()
+		
+		// run the colour switcher
+		colourSwitcher()
 	}
-
-    
-    // what to do when the button is pressed
-    @IBAction func lightChangerButton(_ sender: Any) {
-        
-        // cycle through only 1-3
-        if( colourCounter >= 3 ) {
-            colourCounter = 1
-        } else {
-            colourCounter += 1
-        }
-
-        // run the colour switcher
-        colourSwitcher()
-    }
-    
-    
-    // pass Int 1-3 and change colours based on that
-    func colourSwitcher() {
-        
-        switch colourCounter {
-            case 1:
-                redLight.backgroundColor    = .redOff;
-                yellowLight.backgroundColor = .yellowOff;
-                greenLight.backgroundColor  = .green;
-
-            case 2:
-                redLight.backgroundColor    = .redOff;
-                yellowLight.backgroundColor = .yellow;
-                greenLight.backgroundColor  = .greenOff;
-
-            case 3:
-                redLight.backgroundColor    = .red;
-                yellowLight.backgroundColor = .yellowOff;
-                greenLight.backgroundColor  = .greenOff;
-
-        default:
-                redLight.backgroundColor    = .redOff;
-                yellowLight.backgroundColor = .yellowOff;
-                greenLight.backgroundColor  = .green;
-        }
-        
-
-
-
-    }
-    
+	
+	
+	// what to do when the button is pressed
+	@IBAction func lightChangerButton(_ sender: Any) {
+		
+		// cycle through only 1-3
+		if( colourCounter >= 3 ) {
+			colourCounter = 1
+		} else {
+			colourCounter += 1
+		}
+		
+		// run the colour switcher
+		colourSwitcher()
+	}
+	
+	
+	// pass Int 1-3 and change colours based on that
+	// using switch as tester rather than if/else
+	func colourSwitcher() {
+		
+		switch colourCounter {
+		case 1:
+			// turn red off, turn green on, yellow already off
+			redLight.tintColor    = .redOff;
+			greenLight.tintColor  = .green;
+			
+			buttonText.setTitle("Green light means GO", for: .normal)
+			buttonText.backgroundColor = .greenOff
+			
+		case 2:
+			// turn green off, turn yellow on, red already off
+			yellowLight.tintColor = .yellow;
+			greenLight.tintColor  = .greenOff;
+			
+			buttonText.setTitle("Yellow light means SLOW DOWN", for: .normal)
+			buttonText.backgroundColor = .yellowOff
+			
+		case 3:
+			// turn yellow off, turn red on, green already off
+			redLight.tintColor    = .red;
+			yellowLight.tintColor = .yellowOff;
+			
+			buttonText.setTitle("Red light means STOP", for: .normal)
+			buttonText.backgroundColor = .redOff
+			
+		default:
+			// turn all off to start with
+			redLight.tintColor    = .redOff;
+			yellowLight.tintColor = .yellowOff;
+			greenLight.tintColor  = .greenOff;
+			buttonText.setTitle("Let's Begin", for: .normal)
+		}
+		
+	}
+	
 }
 
